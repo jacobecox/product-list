8ac81392605d577a2b83550c7077d31216f8b362
Jacob Cox <jacobcox@Jacobs-MacBook-Pro.local>
2025-01-12 10:20:06 -0800

    init setup

---
+++ routes/main.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const faker = require("faker");
+const Product = require("../models/product");
+
+router.get("/generate-fake-data", async (req, res, next) => {
+  try {
+    for (let i = 0; i < 90; i++) {
+      const product = new Product({
+        category: faker.commerce.department(),
+        name: faker.commerce.productName(),
+        price: faker.commerce.price(),
+        image: "https://via.placeholder.com/250?text=Product+Image",
+      });
+
+      await product.save();
+      console.log("Product saved:", product);
+    }
+    res.send("Fake data generation complete!");
+  } catch (err) {
+    console.error("Error generating fake data:", err);
+    res.status(500).send("An error occurred while generating fake data.");
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/products");
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+const mainRoutes = require("./routes/main");
+
+app.use(mainRoutes);
+
+app.listen(8000, () => {
+  console.log("Node.js listening on port " + 8000);
+});
