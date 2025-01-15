78854d1a293dd51639f9a3cc21d909732bb2d008
Jacob Cox <jacobcox@Jacobs-MacBook-Pro.local>
2025-01-14 17:13:25 -0800

    working on first api route

--- routes/main.js
+++ routes/main.js
@@ -5,14 +5,12 @@ const Product = require("../models/product");
 router.get("/generate-fake-data", async (req, res, next) => {
   try {
     for (let i = 0; i < 90; i++) {
-      const product = new Product({
+      const product = new Product({ // Create an array of fake products
         category: faker.commerce.department(),
         name: faker.commerce.productName(),
         price: faker.commerce.price(),
         image: "https://via.placeholder.com/250?text=Product+Image",
       });
-
-      console.log('product:', product)
 
       await product.save();
     }
@@ -20,6 +18,32 @@ router.get("/generate-fake-data", async (req, res, next) => {
   } catch (err) {
     console.error("Error generating fake data:", err);
     res.status(500).send("An error occurred while generating fake data.");
+  }
+});
+
+router.get("/products", async (req, res, next) => {
+  try {
+  const perPage = 9;
+
+  const page = req.query.page || 1 // Returns first page by default
+
+  const [products, count] = await Promise.all([
+  Product.find({})
+  .skip(perPage * page - perPage)
+  .limit(perPage),
+  Product.countDocuments()
+  ])
+    Product.countDocuments().exec((err, count) => {
+      if (err) return next(err)
+    })
+    res.json({
+      products,
+      currentPage: page,
+      totalPages: Math.ceil(count / perPage),
+      totalItems: count,
+    });
+  } catch (error) {
+    next(error); // Pass errors to the global error handler
   }
 });
 
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 
 mongoose.connect("mongodb://localhost:27017/products");
 
